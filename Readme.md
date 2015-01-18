e4647568e01c5e221641428b3ad5f21cbc069376
taraniselsu <taraniselsu@gmail.com>
2015-01-17 22:21:49 -0500

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,36 @@
+TAC Archives Fixer
+================
+
+A simple Archives Fixer from Thunder Aerospace Corporation.
+
+For use with the Kerbal Space Program, http://kerbalspaceprogram.com/
+
+Licensed under the Apache License Version 2.0.  See the LICENSE.txt and NOTICE.txt files.
+
+Source code can be found at https://github.com/taraniselsu/TacArchivesFixer
+
+
+### Features
+- Displays information during flight about the current vessel's orbit: apoapsis, periapsis, time to apoapsis, time to periapsis, orbital period, inclination, altitude.
+- The altitude is measured above the surface of the planet, either the terrain or the ocean surface -- whichever is closer.
+- Also shows the current biome.
+- Unlocks in Career mode with the discovery of Basic Rocketry.
+- Does not require any parts.
+- Works during EVA!
+
+
+### Installation instructions
+- Unzip to the KSP directory.
+
+The following files should be in the given locations:
+{KSP}/GameData/ThunderAerospace/TacArchivesFixer/TacArchivesFixer.dll
+{KSP}/GameData/ThunderAerospace/TacArchivesFixer/PluginData/TacArchivesFixer/resize.png
+
+The following file will be created after launching your first spacecraft:
+{KSP}/GameData/ThunderAerospace/TacArchivesFixer/PluginData/TacArchivesFixer/FlightComputer.cfg
+
+
+
+Note that Thunder Aerospace Corporation is a ficticious entity created for entertainment
+purposes. It is in no way meant to represent a real entity. Any similarity to a real entity
+is purely coincidental.
---
+++ Source/TacArchivesFixer.cs
@@ -0,0 +1,67 @@
+﻿/**
+ * TacArchivesFixer.cs
+ * 
+ * Thunder Aerospace Corporation's Archives Fixer for the Kerbal Space Program, by Taranis Elsu
+ * 
+ * (C) Copyright 2015, Taranis Elsu
+ * 
+ * Kerbal Space Program is Copyright (C) 2013 Squad. See http://kerbalspaceprogram.com/. This
+ * project is in no way associated with nor endorsed by Squad.
+ * 
+ * This code is licensed under the Apache License Version 2.0. See the LICENSE.txt and NOTICE.txt
+ * files for more information.
+ * 
+ * Note that Thunder Aerospace Corporation is a ficticious entity created for entertainment
+ * purposes. It is in no way meant to represent a real entity. Any similarity to a real entity
+ * is purely coincidental.
+ */
+
+using KSP.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Tac
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    public class TacArchivesFixer : MonoBehaviour
+    {
+        private bool done = false;
+
+        void Awake()
+        {
+            this.Log("Awake");
+        }
+
+        void Start()
+        {
+            this.Log("Start");
+        }
+
+        void Update()
+        {
+            if (!done && ResearchAndDevelopment.Instance != null)
+            {
+                List<ScienceSubject> subjects = ResearchAndDevelopment.GetSubjects();
+                for (int i = subjects.Count; i == 0; --i)
+                {
+                    ScienceSubject s = subjects[i];
+                    if (s.science == 0.0f)
+                    {
+                        this.LogWarning("Deleting ScienceSubject: " + s.title + " (" + s.id + ") because it has zero science.");
+                        subjects.RemoveAt(i);
+                    }
+                }
+
+                done = true;
+            }
+        }
+
+        void OnDestroy()
+        {
+            this.Log("OnDestroy");
+        }
+    }
+}
